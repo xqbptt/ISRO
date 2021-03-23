8e2a41567bda44c9476dd92feaa4fb4905bee5c7
coolgoel <coolgoel18@gmail.com>
2021-03-23 16:42:44 +0530

    added ISRO observed information to json data

--- data-extractor/index.js
+++ data-extractor/index.js
@@ -8,28 +8,43 @@ const storeData = (data, path) => {
     console.error(err)
   }
 }
-const readCsvStoreJson = (pathToCsvFile, pathToJsonFile) => {
-  csvToJson().fromFile(pathToCsvFile)
-      .then(users => {
-          let data = []
-          users.map((item, id) => {
-              item['id'] = id
-              data.push(item)
-          })
-          storeData(data, pathToJsonFile)
-          console.log("data saved successfully!")
+const readCsvStoreJson = async (pathToCsvFileA, pathToCsvFileB, pathToJsonFile) => {
+  
+  try {
+    const isroElements = await csvToJson().fromFile(pathToCsvFileB)
+    const energySources = await csvToJson().fromFile(pathToCsvFileA)
+
+    let data = []
+    energySources.map((item, id) => {
+      item['id'] = id;
+      item['ISRO details'] = []
+      item['ISRO details count'] = 0
+      isroElements.map((itemO) => {
+        if(itemO['Source_Name'] === item['Observation Name'] && itemO['Source_Name'] !== "") {
+          item['ISRO details'].push(itemO)
+          item['ISRO details count']++;
+        }
       })
-      .catch(err => {
-          console.log(err)
-      })
+      data.push(item)
+    })
+    
+    storeData(data, pathToJsonFile)
+    console.log('data saved successfully!')
+  
+  } catch (err) {
+    console.log(err)
+  }  
 }
 
 let pathToCsvFileHigh = `assets/data/High_Energy.csv`
 let pathToCsvFileLow = `assets/data/Low_Energy.csv`      
 
+
+let pathToCsvFileB = `assets/data/ISROObvB.csv`
+
 let pathToJsonFileHigh = `assets/data/High_Energy.json`
 let pathToJsonFileLow = `assets/data/Low_Energy.json` 
 
-readCsvStoreJson(pathToCsvFileHigh, pathToJsonFileHigh)
-readCsvStoreJson(pathToCsvFileLow, pathToJsonFileLow)
+readCsvStoreJson(pathToCsvFileHigh, pathToCsvFileB, pathToJsonFileHigh)
+readCsvStoreJson(pathToCsvFileLow, pathToCsvFileB, pathToJsonFileLow)
 
