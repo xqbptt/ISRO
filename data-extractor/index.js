272a671a523851125964af6a529a6d87def60302
coolgoel <coolgoel18@gmail.com>
2021-03-23 17:15:44 +0530

    added coordinates in json data

--- data-extractor/index.js
+++ data-extractor/index.js
@@ -19,6 +19,31 @@ const readCsvStoreJson = async (pathToCsvFileA, pathToCsvFileB, pathToJsonFile) 
       item['id'] = id;
       item['ISRO details'] = []
       item['ISRO details count'] = 0
+
+      let delta = 0
+      let alpha = 0
+      for (var key in item) {
+        if(item.hasOwnProperty(key)) {
+          let value = item[key]
+          if(key === 'RA (h)')
+              delta += parseFloat(value) * 15
+          else if(key == 'RA (min)')
+              delta += parseFloat(value) / 4
+          else if(key == 'RA (sec)')
+              delta += parseFloat(value) / 240
+          else if(key == 'DE (deg)')
+              alpha += parseFloat(value)
+          else if(key == 'DE (arcmin)')
+              alpha += parseFloat(value) / 60
+        }
+      }
+      x = Math.cos(delta) * Math.cos(alpha)
+      y = Math.cos(delta) * Math.sin(alpha)
+      z = Math.sin(delta)
+      item['x'] = x
+      item['y'] = y
+      item['z'] = z
+
       isroElements.map((itemO) => {
         if(itemO['Source_Name'] === item['Observation Name'] && itemO['Source_Name'] !== "") {
           item['ISRO details'].push(itemO)
