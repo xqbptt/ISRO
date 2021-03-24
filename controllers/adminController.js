a87f558738ddec41cfe84f5d4d8d793bfd5436c4
s-bhagwat <58619383+s-bhagwat@users.noreply.github.com>
2021-03-24 22:26:24 +0530

    added validations

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -29,16 +29,30 @@ exports.upload = multer({ storage: storage }).fields([
 ]);
 
 exports.csvUploadController = async (req, res) => {
-  const readCsvStoreJson = async (
-    pathToCsvFileA,
-    pathToCsvFileB,
-    pathToJsonFile
-  ) => {
-    let data = [];
-    try {
-      const isroElements = await csvToJson().fromFile(pathToCsvFileB);
-      const energySources = await csvToJson().fromFile(pathToCsvFileA);
+  const isro_observed_map = {};
 
+  const mapIsroElements = (isroElements) => {
+    isroElements.map((item, id) => {
+      const name = item["Source_Name"];
+      if (name !== "" && !isro_observed_map.hasOwnProperty(name)) {
+        isro_observed_map[name] = [];
+        isro_observed_map[name].push(item);
+      } else if (name !== "") {
+        isro_observed_map[name].push(item);
+      }
+    });
+  };
+  const storeData = (data, path) => {
+    try {
+      fs.writeFileSync(path, JSON.stringify(data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const csvStoreJsonOptimised = async (energySources, pathToJsonFile) => {
+    try {
+      let data = [];
       energySources.map((item, id) => {
         item["id"] = id;
         item["ISRO details"] = [];
@@ -56,48 +70,172 @@ exports.csvUploadController = async (req, res) => {
             else if (key == "DE (arcmin)") alpha += parseFloat(value) / 60;
           }
         }
-        x = Math.cos(delta) * Math.cos(alpha);
-        y = Math.cos(delta) * Math.sin(alpha);
-        z = Math.sin(delta);
+        let x = Math.cos(delta) * Math.cos(alpha);
+        let y = Math.cos(delta) * Math.sin(alpha);
+        let z = Math.sin(delta);
         item["x"] = x;
         item["y"] = y;
         item["z"] = z;
-
-        isroElements.map((itemO) => {
-          if (
-            itemO["Source_Name"] === item["Observation Name"] &&
-            itemO["Source_Name"] !== ""
-          ) {
-            item["ISRO details"].push(itemO);
-            item["ISRO details count"]++;
-          }
-        });
+        let obvName = item["Observation Name"];
+        if (obvName !== "" && isro_observed_map.hasOwnProperty(obvName)) {
+          item["ISRO details"].push(...isro_observed_map[obvName]);
+          item["ISRO details count"]++;
+        }
         data.push(item);
       });
+
+      storeData(data, pathToJsonFile);
+      console.log(
+        "data saved successfully! navigate to -----> " + pathToJsonFile
+      );
     } catch (err) {
-      console.log("CSVtoJOSN error");
-      console.log(err);
-    }
-    try {
-      await writeFilePromise(pathToJsonFile, JSON.stringify(data));
-      console.log("data saved successfully!");
-    } catch (err) {
-      console.log("Promisify error");
       console.log(err);
     }
   };
 
-  const pathToCsvFileHigh = `${dirname.dirpath}/assets/data/highEnergyA.csv`;
-  const pathToCsvFileLow = `${dirname.dirpath}/assets/data/lowEnergyA.csv`;
+  let pathToCsvFileHigh = `${dirname.dirpath}/assets/data/highEnergyA.csv`;
+  let pathToCsvFileLow = `${dirname.dirpath}/assets/data/lowEnergyA.csv`;
 
-  const pathToCsvFileB = `${dirname.dirpath}/assets/data/isroObvB.csv`;
+  let pathToCsvFileB = `${dirname.dirpath}/assets/data/isroObvB.csv`;
 
-  const pathToJsonFileHigh = `${dirname.dirpath}/assets/data/highEnergy.json`;
-  const pathToJsonFileLow = `${dirname.dirpath}/assets/data/lowEnergy.json`;
-  await Promise.all([
-    readCsvStoreJson(pathToCsvFileHigh, pathToCsvFileB, pathToJsonFileHigh),
-    readCsvStoreJson(pathToCsvFileLow, pathToCsvFileB, pathToJsonFileLow),
-  ]);
+  let pathToJsonFileHigh = `${dirname.dirpath}/assets/data/highEnergy.json`;
+  let pathToJsonFileLow = `${dirname.dirpath}/assets/data/lowEnergy.json`;
 
+  const driver = async () => {
+    var start, end, time;
+    start = new Date().getTime();
+    const isroElements = await csvToJson().fromFile(pathToCsvFileB);
+    end = new Date().getTime();
+    time = end - start;
+    console.log(
+      "Execution time (in ms) for reading isro observed cosmic sources csv file: " +
+        time +
+        " miliseconds\n"
+    );
+
+    start = new Date().getTime();
+    const highEnergySources = await csvToJson().fromFile(pathToCsvFileHigh);
+    end = new Date().getTime();
+    time = end - start;
+    console.log(
+      "Execution time (in ms) for reading high energy cosmic sources csv file: " +
+        time +
+        " miliseconds\n"
+    );
+
+    start = new Date().getTime();
+    const lowEnergySources = await csvToJson().fromFile(pathToCsvFileLow);
+    end = new Date().getTime();
+    time = end - start;
+    console.log(
+      "Execution time (in ms) for reading low energy cosmic sources csv file: " +
+        time +
+        " miliseconds\n"
+    );
+
+    start = new Date().getTime();
+    mapIsroElements(isroElements);
+    end = new Date().getTime();
+    time = end - start;
+    console.log(
+      "Execution time (in ms) for mapping data: " + time + " miliseconds\n"
+    );
+
+    start = new Date().getTime();
+    csvStoreJsonOptimised(highEnergySources, pathToJsonFileHigh);
+    end = new Date().getTime();
+    time = end - start;
+    console.log(
+      "Execution time (in ms) for processing high energy csv data (optimised code): " +
+        time +
+        " miliseconds\n"
+    );
+
+    start = new Date().getTime();
+    csvStoreJsonOptimised(lowEnergySources, pathToJsonFileLow);
+    end = new Date().getTime();
+    time = end - start;
+    console.log(
+      "Execution time (in ms) for processing low energy csv data (optimised code): " +
+        time +
+        " miliseconds\n"
+    );
+  };
+
+  driver();
   res.render("admin", { name: req.body.name, success: true });
 };
+
+/**
+ * Old code
+ */
+
+//  const readCsvStoreJson = async (
+//   pathToCsvFileA,
+//   pathToCsvFileB,
+//   pathToJsonFile
+// ) => {
+//   let data = [];
+//   try {
+//     const isroElements = await csvToJson().fromFile(pathToCsvFileB);
+//     const energySources = await csvToJson().fromFile(pathToCsvFileA);
+
+//     energySources.map((item, id) => {
+//       item["id"] = id;
+//       item["ISRO details"] = [];
+//       item["ISRO details count"] = 0;
+
+//       let delta = 0;
+//       let alpha = 0;
+//       for (var key in item) {
+//         if (item.hasOwnProperty(key)) {
+//           let value = item[key];
+//           if (key === "RA (h)") delta += parseFloat(value) * 15;
+//           else if (key == "RA (min)") delta += parseFloat(value) / 4;
+//           else if (key == "RA (sec)") delta += parseFloat(value) / 240;
+//           else if (key == "DE (deg)") alpha += parseFloat(value);
+//           else if (key == "DE (arcmin)") alpha += parseFloat(value) / 60;
+//         }
+//       }
+//       x = Math.cos(delta) * Math.cos(alpha);
+//       y = Math.cos(delta) * Math.sin(alpha);
+//       z = Math.sin(delta);
+//       item["x"] = x;
+//       item["y"] = y;
+//       item["z"] = z;
+
+//       isroElements.map((itemO) => {
+//         if (
+//           itemO["Source_Name"] === item["Observation Name"] &&
+//           itemO["Source_Name"] !== ""
+//         ) {
+//           item["ISRO details"].push(itemO);
+//           item["ISRO details count"]++;
+//         }
+//       });
+//       data.push(item);
+//     });
+//   } catch (err) {
+//     console.log("CSVtoJOSN error");
+//     console.log(err);
+//   }
+//   try {
+//     await writeFilePromise(pathToJsonFile, JSON.stringify(data));
+//     console.log("data saved successfully!");
+//   } catch (err) {
+//     console.log("Promisify error");
+//     console.log(err);
+//   }
+// };
+
+// const pathToCsvFileHigh = `${dirname.dirpath}/assets/data/highEnergyA.csv`;
+// const pathToCsvFileLow = `${dirname.dirpath}/assets/data/lowEnergyA.csv`;
+
+// const pathToCsvFileB = `${dirname.dirpath}/assets/data/isroObvB.csv`;
+
+// const pathToJsonFileHigh = `${dirname.dirpath}/assets/data/highEnergy.json`;
+// const pathToJsonFileLow = `${dirname.dirpath}/assets/data/lowEnergy.json`;
+// await Promise.all([
+//   readCsvStoreJson(pathToCsvFileHigh, pathToCsvFileB, pathToJsonFileHigh),
+//   readCsvStoreJson(pathToCsvFileLow, pathToCsvFileB, pathToJsonFileLow),
+// ]);
--- static/admin/index.js
+++ static/admin/index.js
@@ -1,7 +1,38 @@
+// function changeLabelName(str) {
+//   const idName = "file" + str;
+//   let fileName = document.getElementById(idName).value;
+//   fileName = fileName.replace(/.*[\/\\]/, "");
+//   const className = "#file" + str + " + label";
+//   document.querySelector(className).textContent = fileName;
+// }
+function checkFile(filename) {
+  if (filename.includes(".csv")) return true;
+  return false;
+}
 function changeLabelName(str) {
   const idName = "file" + str;
-  let fileName = document.getElementById(idName).value;
+  let fileElement = document.getElementById(idName);
+  let fileName = fileElement.value;
   fileName = fileName.replace(/.*[\/\\]/, "");
   const className = "#file" + str + " + label";
-  document.querySelector(className).textContent = fileName;
+  if (!checkFile(fileName)) {
+    window.alert("Please upload csv file only");
+    //document.querySelector(`#${idName} + label span`);
+    //document.querySelector(`#${idName} + label span`).innerText = "text";
+  } else {
+    document.querySelector(className).textContent = fileName;
+  }
 }
+
+document.getElementById("submitBtn").addEventListener("click", () => {
+  if (
+    document.querySelector("#fileOne + label span").innerText ===
+      "Choose low energy cosmic sources" ||
+    document.querySelector("#fileTwo + label span").innerText ===
+      "Choose high energy cosmic source" ||
+    document.querySelector("#fileThree + label span").innerText ===
+      "Choose astrosat file"
+  ) {
+    window.alert("Please upload all three csv files");
+  }
+});
