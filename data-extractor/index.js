31bf73ea206558e2e365ea6d2293687e20c2ce39
coolgoel <coolgoel18@gmail.com>
2021-03-24 15:54:04 +0530

    optimised data processing code to O(n) using maps

--- data-extractor/index.js
+++ data-extractor/index.js
@@ -1,6 +1,20 @@
 const csvToJson = require('csvtojson');
 const fs = require('fs')
 
+const isro_observed_map = {}
+
+const mapIsroElements = (isroElements) => {
+  isroElements.map((item, id) => {
+    const name = item['Source_Name']
+    if(name !== '' && !isro_observed_map.hasOwnProperty(name)) {
+      isro_observed_map[name] = []
+      isro_observed_map[name].push(item)
+    } 
+    else if(name !== '') {
+      isro_observed_map[name].push(item)
+    }
+  })
+}
 const storeData = (data, path) => {
   try {
     fs.writeFileSync(path, JSON.stringify(data))
@@ -8,11 +22,56 @@ const storeData = (data, path) => {
     console.error(err)
   }
 }
-const readCsvStoreJson = async (pathToCsvFileA, pathToCsvFileB, pathToJsonFile) => {
+// const readCsvStoreJson = async (energySources, isroElements, pathToJsonFile) => {
+  
+//   try {
+
+//     let data = []
+//     energySources.map((item, id) => {
+//       item['id'] = id;
+//       item['ISRO details'] = []
+//       item['ISRO details count'] = 0
+
+//       let delta = 0
+//       let alpha = 0
+//       for (var key in item) {
+//         if(item.hasOwnProperty(key)) {
+//           let value = item[key]
+//           if(key === 'RA (h)')
+//               delta += parseFloat(value) * 15
+//           else if(key == 'RA (min)')
+//               delta += parseFloat(value) / 4
+//           else if(key == 'RA (sec)')
+//               delta += parseFloat(value) / 240
+//           else if(key == 'DE (deg)')
+//               alpha += parseFloat(value)
+//           else if(key == 'DE (arcmin)')
+//               alpha += parseFloat(value) / 60
+//         }
+//       }
+//       x = Math.cos(delta) * Math.cos(alpha)
+//       y = Math.cos(delta) * Math.sin(alpha)
+//       z = Math.sin(delta)
+//       item['x'] = x
+//       item['y'] = y
+//       item['z'] = z
+
+//       isroElements.map((itemO) => {
+//         if(itemO['Source_Name'] === item['Observation Name'] && itemO['Source_Name'] !== "") {
+//           item['ISRO details'].push(itemO)
+//           item['ISRO details count']++;
+//         }
+//       })
+//     })  
+//     storeData(data, pathToJsonFile)
+//     console.log('data saved successfully! filename is' + pathToJsonFile)
+//   } catch (err) {
+//     console.log(err)
+//   }  
+// }
+const csvStoreJsonOptimised = async (energySources, isroElements, pathToJsonFile) => {
   
   try {
-    const isroElements = await csvToJson().fromFile(pathToCsvFileB)
-    const energySources = await csvToJson().fromFile(pathToCsvFileA)
 
     let data = []
     energySources.map((item, id) => {
@@ -37,24 +96,22 @@ const readCsvStoreJson = async (pathToCsvFileA, pathToCsvFileB, pathToJsonFile) 
               alpha += parseFloat(value) / 60
         }
       }
-      x = Math.cos(delta) * Math.cos(alpha)
-      y = Math.cos(delta) * Math.sin(alpha)
-      z = Math.sin(delta)
+      let x = Math.cos(delta) * Math.cos(alpha)
+      let y = Math.cos(delta) * Math.sin(alpha)
+      let z = Math.sin(delta)
       item['x'] = x
       item['y'] = y
       item['z'] = z
-
-      isroElements.map((itemO) => {
-        if(itemO['Source_Name'] === item['Observation Name'] && itemO['Source_Name'] !== "") {
-          item['ISRO details'].push(itemO)
-          item['ISRO details count']++;
-        }
-      })
+      let obvName = item['Observation Name']
+      if(obvName !== '' && isro_observed_map.hasOwnProperty(obvName)) {
+        item['ISRO details'].push(...isro_observed_map[obvName])
+        item['ISRO details count']++;
+      }
       data.push(item)
     })
     
     storeData(data, pathToJsonFile)
-    console.log('data saved successfully!')
+    console.log('data saved successfully! navigate to -----> ' + pathToJsonFile)
   
   } catch (err) {
     console.log(err)
@@ -68,8 +125,63 @@ let pathToCsvFileLow = `assets/data/Low_Energy.csv`
 let pathToCsvFileB = `assets/data/ISROObvB.csv`
 
 let pathToJsonFileHigh = `assets/data/High_Energy.json`
-let pathToJsonFileLow = `assets/data/Low_Energy.json` 
+let pathToJsonFileLow = `assets/data/Low_Energy.json`
 
-readCsvStoreJson(pathToCsvFileHigh, pathToCsvFileB, pathToJsonFileHigh)
-readCsvStoreJson(pathToCsvFileLow, pathToCsvFileB, pathToJsonFileLow)
+const driver = async () => {
+
+  var start, end, time;
+  start = new Date().getTime();
+  const isroElements = await csvToJson().fromFile(pathToCsvFileB)
+  end = new Date().getTime();
+  time = end - start;
+  console.log('Execution time (in ms) for reading isro observed cosmic sources csv file: ' + time + ' miliseconds\n');
+
+  start = new Date().getTime();
+  const highEnergySources = await csvToJson().fromFile(pathToCsvFileHigh)
+  end = new Date().getTime();
+  time = end - start;
+  console.log('Execution time (in ms) for reading high energy cosmic sources csv file: ' + time + ' miliseconds\n');
+
+
+  start = new Date().getTime();
+  const lowEnergySources = await csvToJson().fromFile(pathToCsvFileLow)
+  end = new Date().getTime();
+  time = end - start;
+  console.log('Execution time (in ms) for reading low energy cosmic sources csv file: ' + time + ' miliseconds\n');
+
+  start = new Date().getTime();
+  mapIsroElements(isroElements)
+  end = new Date().getTime();
+  time = end - start;
+  console.log('Execution time (in ms) for mapping data: ' + time + ' miliseconds\n');
+
+  start = new Date().getTime();
+  csvStoreJsonOptimised(highEnergySources, isroElements, pathToJsonFileHigh)
+  end = new Date().getTime();
+  time = end - start;
+  console.log('Execution time (in ms) for processing high energy csv data (optimised code): ' + time + ' miliseconds\n');
+
+  start = new Date().getTime();
+  csvStoreJsonOptimised(lowEnergySources, isroElements, pathToJsonFileLow)
+  end = new Date().getTime();
+  time = end - start;
+  console.log('Execution time (in ms) for processing low energy csv data (optimised code): ' + time + ' miliseconds\n');
+
+  // start = new Date().getTime();
+  // readCsvStoreJson(highEnergySources, isroElements, pathToJsonFileHigh)
+  // end = new Date().getTime();
+  // time = end - start;
+  // console.log('Execution time (in ms) for processing high energy csv data (brute force code): ' + time + ' miliseconds\n');
+
+  // start = new Date().getTime();
+  // readCsvStoreJson(lowEnergySources, isroElements, pathToJsonFileLow)
+  // end = new Date().getTime();
+  // time = end - start;
+  // console.log('Execution time (in ms) for processing low energy csv data (brute force code): ' + time + ' miliseconds\n');
+
+}
+
+driver()
+ 
+
 
